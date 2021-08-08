d2d76fe9e9107bf24f76691855b33fc20876b459
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-08-08 16:01:04 -0500

    feat: created custom component card and have wrapper inherit css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,5 @@
 import './ExpenseItem.css';
-import './ExpenseDate';
 import ExpenseDate from "./ExpenseDate";
-import './Card.css'
 import Card from "./Card";
 function ExpenseItem(props){
 
@@ -12,7 +10,8 @@ function ExpenseItem(props){
             <h2>{props.title}</h2>
             <div className="expense-item__price">{props.amount}</div>
         </div>
-    </Card>);
+    </Card>
+    );
 }
 
 export default ExpenseItem; //need to export the file so its usable outside of its home file
